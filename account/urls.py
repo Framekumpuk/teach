1fb5073534190d9bdaeeb3065095b806bf1469df
Framekumpuk <gunzritzicez_ts6@hotmail.com>
2017-03-18 17:02:12 +0000

    Writing more views

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'account'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^detail$', views.operand, name='operand' ),
+    url(r'^history$', views.history, name='history' ),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Account
+from .forms import UploadFileForm
+import csv
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request,"account/index.html",'')
+    
+def operand(request):
+    error_message = 0 
+    try:
+        paylist = request.POST['paylist']
+        data = request.POST['data']
+        money = request.POST['value']
+        date = request.POST['date']
+        if paylist != 'income':
+            money = int(money)*-1
+    except:
+        error_message = 1
+        data = ""
+        money = ""
+        date = ""
+    else:
+            information = Account(account_text=data, account_money=money, pub_date=date)
+            information.save()
+    # user hits the Back button.
+    return render(request,"account/detail.html",'')
+
+def history(request):
+    payment_list = Account.objects.order_by('-pub_date')
+    total = 0
+    for money in payment_list:
+        total = total + money.account_money
+    # Calculate balance of paylist.
+    return render(request,"account/history.html",{'payment_list':payment_list, 'amt_total':total})
